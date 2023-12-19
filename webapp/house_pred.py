e9cabf7a3b841debdccc7f4a056cf169aa8108d1
Michael Shteyn <5659756+mshteyn@users.noreply.github.com>
2023-12-19 02:03:10 -0500

    Add files via upload

--- webapp/house_pred.py
+++ webapp/house_pred.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 import torch
+import numpy as np
 import torchvision.transforms as transforms
 from torchvision import models
 from PIL import Image
@@ -17,10 +18,23 @@ def home():
 @app.route('/predict', methods=['POST'])
 def get_predictiction():
     image = request.files['image']
-    new_img = io.imread(image)
-    prediction = predict(new_img)
-    print(prediction, sep = "\n")
+    new_img = io.imread(image) 
     io.imsave('static/user_img.jpg', new_img)
+
+    #identify a house gestalt
+    label = check_imagenet(new_img)
+    acceptable_categories = ['house', 'home', 'palace', 'church', 'mosque', 'castle', 'barn']
+    is_house = any([(AC in label) for AC in acceptable_categories])
+
+    #classify houses only
+    if is_house == True:
+        prediction = predict(new_img)
+        print(prediction, sep = "\n")
+    else:
+        prediction  = ' It appears you may have uploaded an image of a ' + label.upper() +'.\n Please make sure to upload an image of a HOUSE.\n\n'
+
+    store_image(new_img)
+    #io.imsave('static/user_img.jpg', new_img)
 
     #return prediction
     return render_template('predict.html', image = new_img, p1=prediction.split('\n')[0],
@@ -28,16 +42,22 @@ def get_predictiction():
 
 
 
-def transform_image(image):
+def transform_image(image, test):
         #image = io.imread(img_path)
-        image = resize(image,(232,320),anti_aliasing=True) # for RESNET50 model
-        #image=resize(image,(384,384),anti_aliasing=True) #for vit_B model
-        #image=resize(image,(256,256),anti_aliasing=True) #for vit_L model
-        # apply_TF= transforms.Compose([transforms.Normalize( # Normalize for Nrm network
-        #                                     [0.485, 0.456, 0.406],
-        #                                     [0.229, 0.224, 0.225])])
-        # tf_image = apply_TF(torch.tensor(image.T))
-        tf_image = torch.tensor(image.T)
+        if test == 'IMAGENET':
+            apply_TF= transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),                                        
+                                        transforms.Normalize( # Normalize for RESNET50(?)
+                                                [0.485, 0.456, 0.406],
+                                                [0.229, 0.224, 0.225])])
+
+            image = Image.fromarray(np.uint8(image)).convert('RGB')      
+            tf_image = apply_TF(image)
+
+        else:
+            image = resize(image,(232,320),anti_aliasing=True) # for RESNET50 model
+            tf_image = torch.tensor(image.T)
 
         #conform data shape to model
         rtf_image = tf_image.unsqueeze(0).float()        
@@ -46,7 +66,7 @@ def transform_image(image):
 
 
 def predict(img_path):
-    rtf_image = transform_image(img_path) # get it into image form
+    rtf_image = transform_image(img_path, test ='fine_tune') # get it into image form
     outputs = model.eval()
     scores = model.forward(rtf_image)
     #feature_names = ['fair market value (usd)', 'year built']
@@ -68,8 +88,32 @@ def predict(img_path):
     print(out)
     return out
 
+def check_imagenet(new_img):
+    inet_model = models.resnet50(pretrained=True)
+    inet_model.eval()
 
+    image = new_img
+    img_tensor = transform_image(image, 'IMAGENET')
+    inet_model.eval()
+    outputs = inet_model.forward(img_tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    import json
+    imagenet_class_index = json.load(open('imagenet_class_index.json'))    
+    object_id = imagenet_class_index[predicted_idx][1]
 
+    return object_id    
+
+def store_image(new_img):
+    #resize image prior to saving
+    if new_img.shape[0]>500 or new_img.shape[1]>500:
+        if new_img.shape[0]>1.2*new_img.shape[1]:
+            new_img = resize(new_img, (232, int(232*1.3)),anti_aliasing=True)
+        elif new_img.shape[1]>1.2*new_img.shape[0]:
+            new_img = resize(new_img, (int(232*1.3), 232),anti_aliasing=True)
+        else:
+            new_img = resize(new_img, (232, 232),anti_aliasing=True)
+    io.imsave('static/user_img.jpg', new_img)
 # load model
 # zipped dict.
 #zip_dict = '../saved_mods/saved_mod_resenet50_FMV_YBT_FLA_64bs_e7.zip'
